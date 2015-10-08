060ecce6b0e8f1e2ed0c55fa6c90b1755fd6bb87
Michael Stewart <tgm@vt.edu>
2015-10-08 10:30:08 -0400

    credit where it's due

--- inclusion/app/scripts/controllers/about.js
+++ inclusion/app/scripts/controllers/about.js
@@ -8,10 +8,16 @@
  * Controller of the inclusionApp
  */
 angular.module('inclusionApp')
-  .controller('AboutCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomePeople = [
+      'Karen DePauw',
+      'Dannette Gomez Beane',
+      'Marcy Schnitzer',
+      'Stephanie House-Niamke',
+      'Roxanna Link',
+      'Isabella Sereno Berrizbeitia',
+      'Maria Elisa Christie',
+      'Nathan Self',
+      'Michael Stewart',
     ];
   });
